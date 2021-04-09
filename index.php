90ef3edc37b8badf9b4bd13c48d5d92cdbea75ec
NickSchaad <nkschaad@gmail.com>
2021-04-08 22:36:24 -0700

    added basic, functioning hot posts table on index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
         /* added temporary styles for visualization :) */
         #table-test td{border: 1px solid black;}
         #table-test{border: 1px solid black; margin-left: auto; margin-right: auto; width: 80%; text-align: center;}
-        h1 {text-align: center;}
+        h1, h2{text-align: center;}
         
     </style>
     <?php include('head.php'); ?>
@@ -31,6 +31,7 @@ echo"</tr>";
 //make connection to database
 include 'dbhosts.php';
 $connection = mysqli_connect($host, $user, $password, $database);
+$connection2 = mysqli_connect($host, $user, $password, $database);
 
 $error = mysqli_connect_error();
 if($error != null) {
@@ -55,9 +56,50 @@ if (mysqli_num_rows($result) > 0) {
     }
 }
 echo"</table>";
-//close connection.
 
+$sql = "select count(replies.ID) as numReply, replyPostId from replies where replyDate >= DATE_ADD(CURDATE(), INTERVAL -1 DAY) group by replyPostId ORDER BY numReply DESC LIMIT 2";
+$result = mysqli_query($connection, $sql);
+if (mysqli_num_rows($result) > 0) {
+    echo"<h2 style = >Hot Posts!!!</h2>";
+    echo"<table id = 'table-test'>";
+    echo"<tr>";
+    echo"<th>Category</th>";
+    echo"<th>Title</th>";
+    echo"<th>Author</th>";
+    echo"<th>Post Date</th>";
+    echo"<th>Replies</th>";
+    
+    echo"</tr>";
+    while($row = mysqli_fetch_assoc($result)) {
+        echo"<tr>";
+        $tempNumReply = $row["numReply"];
+        $tempPostID= $row["replyPostId"];
+        $sql2 = "SELECT categoryName, category.ID, username, title, DATE_FORMAT(postDate, '%M %e, %Y %l:%i %p') AS postDate FROM category JOIN posts ON category.ID = posts.postCategoryId JOIN users ON users.ID = posts.postUserId WHERE posts.ID =$tempPostID";
+        $result2 = mysqli_query($connection2, $sql2);
+        
+        while($row2 = mysqli_fetch_assoc($result2)){
+        $tempCategoryName = $row2["categoryName"];
+        $tempTitle = $row2["title"];
+        $tempUsername = $row2["username"];
+        $tempPostDate = $row2["postDate"];
+        
+        }
+        echo"<td>$tempCategoryName</td>";
+        echo"<td><a href='forumReply.php?postID=$tempPostID'</a>$tempTitle</td>";
+        echo"<td>$tempUsername</td>";
+        echo"<td>$tempPostDate</td>";
+        echo"<td>$tempNumReply</td>";
+        echo"</tr>";
+        
+    }
+    echo"</table>";
+}
+
+mysqli_free_result($result);
+mysqli_free_result($result2);
+//close connection.
 mysqli_close($connection);
+mysqli_close($connection2);
 
 ?>
 
